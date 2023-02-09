84d89175ac236f8959eb384d0217e0755ab9b99c
Dylan Blackwell <tophatrev@gmail.com>
2023-02-09 14:56:45 +0000

    Add functioning chart and voting.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+function getRandomProductIndex() {
+  return Math.floor(Math.random() * Product.allProducts.length);
+}
+
 let totalClicks = 0;
 const maxClicks = 10;
-
 function Product(name, src) {
   this.name = name;
   this.src = src;
-  this.views = 0;
   this.clicks = 0;
-  Product.allProductsArray.push(this);
+  this.views = 0;
+  Product.allProducts.push(this);
 }
 
-Product.allProductsArray = [];
+Product.allProducts = [];
 
 const productNames = [
   "bag",
@@ -36,47 +39,118 @@ for (let i = 0; i < productNames.length; i++) {
   new Product(productNames[i], `images/${productNames[i]}.jpeg`);
 }
 
-function randomNum() {
-  return Math.floor(Math.random() * Product.allProductsArray.length);
-}
-
+// render our products onto the page
 function renderProducts() {
-  let product1 = randomNum();
-  let product2 = randomNum();
-  let product3 = randomNum();
+  // get three random product indexes
+  let product1 = getRandomProductIndex();
+  let product2 = getRandomProductIndex();
+  let product3 = getRandomProductIndex();
 
+  // check none of them match
   while (
     product1 === product2 ||
     product1 === product3 ||
     product2 === product3
   ) {
-    product2 = randomNum();
-    product3 = randomNum();
+    product2 = getRandomProductIndex();
+    product3 = getRandomProductIndex();
   }
 
-  let img1 = document.getElementById("img1");
-  let img2 = document.getElementById("img2");
-  let img3 = document.getElementById("img3");
+  // put those images onto the page
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  const img3 = document.getElementById("img3");
 
-  img1.src = Product.allProductsArray[product1].src;
-  img1.name = Product.allProductsArray[product1].name;
+  img1.src = Product.allProducts[product1].src;
+  img1.alt = Product.allProducts[product1].name;
 
-  img2.src = Product.allProductsArray[product2].src;
-  img2.name = Product.allProductsArray[product2].name;
+  img2.src = Product.allProducts[product2].src;
+  img2.alt = Product.allProducts[product2].name;
 
-  img3.src = Product.allProductsArray[product3].src;
-  img3.name = Product.allProductsArray[product3].name;
+  img3.src = Product.allProducts[product3].src;
+  img3.alt = Product.allProducts[product3].name;
+
+  // increase views for displayed images
+  Product.allProducts[product1].views++;
+  Product.allProducts[product2].views++;
+  Product.allProducts[product3].views++;
 }
 
-renderProducts();
-
+// listen for clicks on the images
 function handleClick(event) {
+  // make sure they are clicking on an image and not the container itself
   if (event.target === imgContainer) {
-    alert("Nuh uh, click the image you buffoon.");
+    alert("You've got to click on the image!");
   } else {
     totalClicks++;
+  }
+
+  // increase clicks
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    if (Product.allProducts[i].name === event.target.alt) {
+      Product.allProducts[i].clicks++;
+      break; // ends the for loop
+    }
+  }
+
+  // check max clicks
+  if (totalClicks === maxClicks) {
+    // remove the event listener so the game ends
+    imgContainer.removeEventListener("click", handleClick);
+    // maybe rnder results?
+    renderChart();
+  } else {
+    renderProducts();
+  }
+}
+
+function renderResults() {
+  const resultsList = document.getElementById("results-list");
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    let theProduct = Product.allProducts[i];
+    let li = document.createElement("li");
+    li.textContent = `${theProduct.name}: ${theProduct.clicks} clicks ${theProduct.views} views`;
+    resultsList.appendChild(li);
   }
 }
 
 const imgContainer = document.getElementById("img-container");
 imgContainer.addEventListener("click", handleClick);
+
+function renderChart() {
+  const myChart = document.getElementById("chart");
+  let labels = [];
+  let viewsData = [];
+  let clicksData = [];
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    labels.push(Product.allProducts[i].name);
+    viewsData.push(Product.allProducts[i].views);
+    clicksData.push(Product.allProducts[i].clicks);
+  }
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "# of Views",
+        data: viewsData,
+        borderWidth: 1,
+      },
+      {
+        label: "# of Votes",
+        data: clicksData,
+        borderWidth: 1,
+      },
+    ],
+  };
+  const config = {
+    type: "bar",
+    data: data,
+  };
+
+  new Chart(myChart, config);
+}
+
+renderProducts();
